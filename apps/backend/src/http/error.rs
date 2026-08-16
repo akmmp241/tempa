ff66232836cd7488bfaccd20972cb07a5a24241a
Akmal Muhammad Pridianto <akmalmp241@gmail.com>
2026-08-16 20:55:15 +0700

    feat: enhance HttpError enum with additional variants and improved response handling

--- apps/backend/src/http/error.rs
+++ apps/backend/src/http/error.rs
@@ -2,11 +2,21 @@ use axum::Json;
 use axum::response::{IntoResponse, Response};
 use http::StatusCode;
 use serde_json::json;
+use thiserror::Error;
+use validator::ValidationErrors;
 
+#[derive(Debug, Error)]
 pub enum HttpError {
+    #[error("not found: {0:?}")]
     NotFound(String),
+    #[error("bad request: {0:?}")]
     BadRequest(String),
-    InternalServerError,
+    #[error("Validation Error: {0:?}")]
+    Validation(#[from] ValidationErrors),
+    #[error("Unauthorized")]
+    Unauthorized,
+    #[error("Internal Server Error")]
+    InternalServerError(#[from] anyhow::Error),
 }
 
 impl IntoResponse for HttpError {
@@ -20,7 +30,6 @@ impl IntoResponse for HttpError {
                 })),
             )
                 .into_response(),
-
             HttpError::BadRequest(message) => (
                 StatusCode::BAD_REQUEST,
                 Json(json!({
@@ -29,14 +38,33 @@ impl IntoResponse for HttpError {
                 })),
             )
                 .into_response(),
-
-            HttpError::InternalServerError => (
-                StatusCode::INTERNAL_SERVER_ERROR,
+            HttpError::Validation(message) => (
+                StatusCode::BAD_REQUEST,
                 Json(json!({
                     "success": false,
-                    "message": "Internal server error"
+                    "message": message
                 })),
             )
+                .into_response(),
+
+            HttpError::Unauthorized => (
+                StatusCode::UNAUTHORIZED,
+                Json(json!({
+                    "success": false,
+                    "message": "Unauthorized"
+                })),
+            )
+                .into_response(),
+            HttpError::InternalServerError(msg) => {
+                log::error!("internal server error: {}", msg);
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    Json(json!({
+                        "success": false,
+                        "message": "Internal server error"
+                    })),
+                )
+            }
                 .into_response(),
         }
     }
